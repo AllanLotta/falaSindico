8b6e053964f9953b36a9dd638eca874be694bfec
Allan Lotta <allanmatheus77@outlook.com>
2019-11-23 06:21:13 -0300

    added check is_sindico on menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,6 +2,7 @@ import React, {useContext, useEffect} from 'react';
 import {Text, SafeAreaView, TouchableOpacity, FlatList} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {MenuContext} from '../../services/MenuContext';
+import {DataContext} from '../../services/DataContext';
 import LogoIcon from '../../assets/logo2.png';
 import {Container, Header, List, Item, TextItem, Logo} from './styles';
 
@@ -14,11 +15,20 @@ const MenuItens = [
   {id: '06', text: 'Enviar Mensagem', icon: 'ios-chatbubbles'},
   {id: '07', text: 'Mudar de Prédio', icon: 'ios-log-out'},
 ];
+const MenuItens2 = [
+  {id: '01', text: 'Meu Condomínio', icon: 'md-grid'},
+  {id: '02', text: 'Notícias', icon: 'ios-paper'},
+  {id: '03', text: 'Fala Síndico Clube', icon: 'ios-briefcase'},
+  {id: '04', text: 'Classificados', icon: 'ios-clipboard'},
+  {id: '05', text: 'Arquivos', icon: 'ios-folder'},
+  {id: '07', text: 'Mudar de Prédio', icon: 'ios-log-out'},
+];
 
 export default function Menu() {
   const [menu, setMenu, activeRouter, setActiveRouter] = useContext(
     MenuContext
   );
+  const [data, setData, cod, setCod] = useContext(DataContext);
 
   // useEffect(() => {
   //   setActiveRouter(MenuItens[0].text);
@@ -42,7 +52,7 @@ export default function Menu() {
           <Text />
         </Header>
         <List
-          data={MenuItens}
+          data={data.is_sindico ? MenuItens : MenuItens2}
           keyExtractor={itemK => itemK.id}
           renderItem={({item}) => (
             <Item onPress={() => setActive(item.text)}>
